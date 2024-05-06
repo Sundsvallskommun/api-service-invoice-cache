6b304ac0b388c2079d2ffd107416eed141e1e8f3
Joakim Schürer <joakim.schurer@sundsvall.se>
2024-05-06 10:23:37 +0200

    UF-8181: Removed server information
    - Removed server information from swagger
    - Modified shedlock config to use db time
    - Added tests for configuration classes
    - Adjusted autorization token to use dummy value
    - Extended wiremocks to include autorization token in request match

--- src/integration-test/java/apptest/InvoiceCacheIT.java
+++ src/integration-test/java/apptest/InvoiceCacheIT.java
@@ -12,8 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MSSQLServerContainer;
---
+++ src/test/java/se/sundsvall/invoicecache/integration/smb/SMBPropertiesTest.java
@@ -0,0 +1,28 @@
+package se.sundsvall.invoicecache.integration.smb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import se.sundsvall.invoicecache.InvoiceCache;
+
+@SpringBootTest(classes = InvoiceCache.class)
+@ActiveProfiles("junit")
+class SMBPropertiesTest {
+
+	@Autowired
+	private SMBProperties properties;
+
+	@Test
+	void verifyProperties() {
+		assertThat(properties.getDomain()).isEqualTo("smb-domain");
+		assertThat(properties.getPassword()).isEqualTo("smb-password");
+		assertThat(properties.getRemoteDir()).isEqualTo("smb-remote-dir");
+		assertThat(properties.getShareAndDir()).isEqualTo("smb-share-and-dir");
+		assertThat(properties.getUser()).isEqualTo("smb-user");
+		assertThat(properties.getUserDomain()).isEqualTo("smb-user-domain");
+	}
+}
