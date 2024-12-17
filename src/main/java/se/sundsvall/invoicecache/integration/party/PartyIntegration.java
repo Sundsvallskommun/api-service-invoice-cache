5b321187a264483f50589985c2e073d13b77ca63
cheezi0747 <linus.sjolinder@b3.se>
2024-12-17 10:42:25 +0100

    UF-11348: Add Circuitbreakers

--- src/main/java/se/sundsvall/invoicecache/integration/party/PartyIntegration.java
+++ src/main/java/se/sundsvall/invoicecache/integration/party/PartyIntegration.java
@@ -2,6 +2,7 @@ package se.sundsvall.invoicecache.integration.party;
 
 import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static se.sundsvall.invoicecache.integration.party.PartyConfiguration.REGISTRATION_ID;
 
 import generated.se.sundsvall.party.PartyType;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-	name = "party",
+	name = REGISTRATION_ID,
 	url = "${integration.party.url}",
 	configuration = PartyConfiguration.class,
 	dismiss404 = true)
-@CircuitBreaker(name = "party")
+@CircuitBreaker(name = REGISTRATION_ID)
 public interface PartyIntegration {
 
 	/**
