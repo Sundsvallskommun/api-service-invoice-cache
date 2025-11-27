864c5fe53853be32f0900e0f1086b7273f3a5f74
AndreSoderlund <andre.soderlund@b3.se>
2025-11-27 08:29:34 +0100

    Added a scheduled job that removes raindance invoices that are older than 18 months.

---
+++ src/main/java/se/sundsvall/invoicecache/service/scheduler/RemovalWorker.java
@@ -0,0 +1,39 @@
+package se.sundsvall.invoicecache.service.scheduler;
+
+import static org.zalando.problem.Status.INTERNAL_SERVER_ERROR;
+import static se.sundsvall.invoicecache.Constant.RAINDANCE_ISSUER_LEGAL_ID;
+
+import java.time.OffsetDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.zalando.problem.Problem;
+import se.sundsvall.invoicecache.integration.db.PdfRepository;
+
+@Component
+public class RemovalWorker {
+
+	private static final Logger LOG = LoggerFactory.getLogger(RemovalWorker.class);
+	private final PdfRepository pdfRepository;
+
+	public RemovalWorker(final PdfRepository pdfRepository) {
+		this.pdfRepository = pdfRepository;
+	}
+
+	@Transactional
+	public void removeOldRaindanceInvoices() {
+		var cutoff = OffsetDateTime.now().minusMonths(18L);
+		var numberOfOldRaindancePdfs = pdfRepository.countAllByInvoiceIssuerLegalIdAndCreatedIsBefore(RAINDANCE_ISSUER_LEGAL_ID, cutoff);
+		LOG.info("Found {} old Raindance PDFs to be removed.", numberOfOldRaindancePdfs);
+
+		try {
+			var numberOfRemovedPdfs = pdfRepository.deleteAllOldRaindancePdfs(RAINDANCE_ISSUER_LEGAL_ID, cutoff);
+			LOG.info("Deleted {} old Raindance PDFs.", numberOfRemovedPdfs);
+		} catch (Exception e) {
+			LOG.error("Error occurred while removing old Raindance PDFs: {}", e.getMessage());
+			throw Problem.valueOf(INTERNAL_SERVER_ERROR, "Failed to remove old Raindance PDFs.");
+		}
+	}
+
+}
---
+++ src/test/java/se/sundsvall/invoicecache/service/scheduler/RemovalSchedulerTest.java
@@ -0,0 +1,34 @@
+package se.sundsvall.invoicecache.service.scheduler;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RemovalSchedulerTest {
+
+	@Mock
+	private RemovalWorker removalWorkerMock;
+
+	@InjectMocks
+	private RemovalScheduler removalScheduler;
+
+	@AfterEach
+	void tearDown() {
+		verifyNoMoreInteractions(removalWorkerMock);
+	}
+
+	@Test
+	void removeOldRaindanceInvoices() {
+		removalScheduler.removeOldRaindanceInvoices();
+
+		verify(removalWorkerMock).removeOldRaindanceInvoices();
+	}
+
+}
